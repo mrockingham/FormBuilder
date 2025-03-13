b3959d364e93a78eb0a7d445273ee35e998b89cb
Michael Rockingham <mrockingham@campbellmarketing.com>
2025-03-13 12:57:09 -0400

    updates on w

--- formbuilder/src/components/sidebar/Elements/SideBarTextInputBtn.tsx
+++ formbuilder/src/components/sidebar/Elements/SideBarTextInputBtn.tsx
@@ -57,7 +57,7 @@ const SideBarTextInputBtn = () => {
               >
                 <MdPowerInput
                   style={{
-                    border: "1px solid limegreen",
+                    border: "1px solid dodgerblue",
                     width: "30px",
                     boxShadow: `16px 10px 40px #5e625c,
                     -16px -10px 32px #ffffff`,
@@ -98,7 +98,7 @@ const SideBarTextInputBtn = () => {
               >
                 <CiTextAlignJustify
                   style={{
-                    border: "1px solid limegreen",
+                    border: "1px solid dodgerblue",
                     width: "30px",
                     boxShadow: `16px 10px 45px #5e625c,
                     -16px -10px 45px #ffffff`,
